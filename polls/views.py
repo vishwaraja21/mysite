c8d5a87446dda1eec3af54627761542cd1b077a9
vishwaraja21 <102411421+vishwaraja21@users.noreply.github.com>
2024-10-18 12:18:32 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -57,4 +57,4 @@ def vote(request, question_id):
 
 # New owner view function
 def owner(request):
-    return HttpResponse("Hello, world. c433ceb4 is the polls index.")
+    return HttpResponse("Hello, world. 619aaecd is the polls index.")
