e3f64e472c8268d0bd9bec90d1c1bd4fb7c6c6d3
vishwaraja21 <102411421+vishwaraja21@users.noreply.github.com>
2024-10-18 17:17:44 +0530

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('owner', views.owner, name='owner'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    # New owner URL pattern
-    path('owner/', views.owner, name='owner'),
 ]
